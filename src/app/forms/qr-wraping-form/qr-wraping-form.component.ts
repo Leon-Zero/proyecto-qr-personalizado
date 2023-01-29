52a536478cc1b2a1a12c1f0424db1b9f860b2c71
Leon-Zero <leon.cba09@gmail.com>
2023-01-28 23:35:51 -0300

    nuevos iconos y marcos

--- src/app/forms/qr-wraping-form/qr-wraping-form.component.ts
+++ src/app/forms/qr-wraping-form/qr-wraping-form.component.ts
@@ -10,26 +10,34 @@ export class QrWrapingFormComponent implements OnInit {
   wraping = "not_selection";
   wrapingClass = {
     "wraping": [
-      { "name": "not_selection" }, { "name": "party" }, { "name": "neon" }, { "name": "movil" }, { "name": "rabbit" }, { "name": "laser" },
-      { "name": "china" }, { "name": "rustico" }, {"name":"video"}, { "name": "road" }, { "name": "scan" },
-      { "name": "colors" }, { "name": "estambre" }, { "name": "bear" }, { "name": "black-ig" }
+      { "name": "not_selection" }, { "name": "party" }, { "name": "neon" }, { "name": "movil" },
+      { "name": "rabbit" }, { "name": "ciervo" }, { "name": "lobo" }, { "name": "oso_pardo" }, { "name": "pinguino" }, { "name": "pig" },
+      { "name": "dog" }, { "name": "mapache" }, { "name": "laser" }, { "name": "china" },
+      { "name": "video" }, { "name": "road" }, { "name": "scan" }, { "name": "colors" },
+      { "name": "estambre" }, { "name": "bear" }, { "name": "black-ig" }, { "name": "twitter" },
+      { "name": "shop" }, { "name": "dinner" }
     ]
   }
 
   constructor(public qrService: QrValuesService) { }
 
   ngOnInit(): void {
-    this.qrService.observableWraping.subscribe(response =>{
+    this.qrService.observableWraping.subscribe(response => {
       this.wraping = response;
     });
   }
 
   sendWraping(wraping: string) {
-    if(wraping === "not_selection"){
-    this.qrService.sendSizeWrap(18);
+    if (wraping === "not_selection") {
+      this.qrService.sendWraping(wraping);
+      this.qrService.sendSizeWrap(15);
     }
-    this.qrService.sendWraping(wraping);
-    this.qrService.sendSizeWrap(26);
+    if (this.qrService.sizeWrapValue > 26) {
+      this.qrService.sendWraping(wraping);
+    } else {
+      this.qrService.sendWraping(wraping);
+      this.qrService.sendSizeWrap(26);
+    }
   }
 
 }
